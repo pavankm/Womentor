e3b74461174dd79b8f930245dad2b8a0bc51a131
pavankm <kpavan@protonmail.com>
2018-03-31 19:03:04 -0500

    [FIX] Already logged in users should not be asked to login or register again

--- app/src/main/java/me/ancyphilip/womentor/ChooseLoginOrRegistrationActivity.java
+++ app/src/main/java/me/ancyphilip/womentor/ChooseLoginOrRegistrationActivity.java
@@ -1,19 +1,30 @@
 package me.ancyphilip.womentor;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ChooseLoginOrRegistrationActivity extends AppCompatActivity {
 
     private Button mLogin, mRegister;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_login_or_registration);
+        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.getCurrentUser() != null) {
+            //already logged in
+            Intent intent = new Intent(ChooseLoginOrRegistrationActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
 
         mLogin = (Button) findViewById(R.id.login);
         mRegister = (Button) findViewById(R.id.register);
